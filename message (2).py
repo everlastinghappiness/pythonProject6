bb500e2e8217cfeefe7488580fd9b64876612608
student <daniillit2007@gmail.com>
2023-12-29 15:03:24 +0900

    Car class

--- message (2).py
+++ message (2).py
@@ -1,84 +1,85 @@
 import pygame
-import random
 
-# Инициализация Pygame
+import sys
+#import os
+
 pygame.init()
-
-# Определение цветов
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-
 # Определение размеров окна
 WINDOW_WIDTH = 600
-WINDOW_HEIGHT = 800
+WINDOW_HEIGHT = 600
 
 # Создание окна
-window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Гонки")
 
-# Загрузка изображений
-car_image = pygame.image.load("car.png")
-car_width = car_image.get_width()
-car_height = car_image.get_height()
 
-# Определение скорости и направления движения машин
-player_speed = 0
-enemy_speed = 5
-enemy_directions = [-1, 0, 1]
+def terminate():
+    pygame.quit()
+    sys.exit()
 
-# Определение начальных позиций машин
-player_x = WINDOW_WIDTH / 2 - car_width / 2
-player_y = WINDOW_HEIGHT - car_height - 50
-enemy_x = random.randint(0, WINDOW_WIDTH - car_width)
-enemy_y = -car_height
 
-# Определение счета игрока
+x = 50
+speed = 10
+
 score = 0
+
+
+class Car(pygame.sprite.Sprite):
+    def __init__(self, all_sprites):
+        super().__init__(all_sprites)
+        self.image = pygame.image.load("car.png")
+        self.car_image = pygame.image.load("car.png")
+
+        self.rect = self.car_image.get_rect()
+        self.rect.x = 10
+        self.rect.y = 200
+
+        self.player_x = WINDOW_WIDTH / 2 - self.rect.width / 2
+        self.player_y = WINDOW_HEIGHT - self.rect.height - 50
+
+    def update(self, player_x):
+        self.rect.left += speed
+
 
 # Создание шрифта
 font = pygame.font.Font(None, 36)
 
-# Основной игровой цикл
-running = True
-while running:
-    # Обработка событий
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                player_speed = -5
-            elif event.key == pygame.K_RIGHT:
-                player_speed = 5
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                player_speed = 0
 
-    # Обновление позиции машин
-    player_x += player_speed
-    enemy_x += enemy_directions[random.randint(0, 2)] * enemy_speed
+def main():
+    # Отрисовка объектов на экране
+    screen.fill('white')
 
-    # Проверка столкновения машин
-    if enemy_y + car_height > player_y and enemy_y < player_y + car_height:
-        if enemy_x + car_width > player_x and enemy_x < player_x + car_width:
-            running = False
+    all_sprites = pygame.sprite.Group()
+
+    car = Car(all_sprites)
+
+    score_text = font.render("Score: " + str(score), True, 'black')
+    score_exampl = font.render('2' + str(score), True, 'black')
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            key = pygame.key.get_pressed()
+            if key[pygame.K_RIGHT]:
+                car.update(x)
+        screen.fill('white')
+        all_sprites.draw(screen)
+        screen.blit(score_text, (10, 10))
+        screen.blit(score_exampl, (300, 0))
+        pygame.display.update()
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
 
     # Обновление счета игрока
-    if enemy_y > WINDOW_HEIGHT:
-        score += 1
-        enemy_x = random.randint(0, WINDOW_WIDTH - car_width)
-        enemy_y = -car_height
-
-    # Отрисовка объектов на экране
-    window.fill(WHITE)
-    window.blit(car_image, (player_x, player_y))
-    window.blit(car_image, (enemy_x, enemy_y))
-    score_text = font.render("Score: " + str(score), True, BLACK)
-    window.blit(score_text, (10, 10))
-
-    # Обновление экрана
-    pygame.display.update()
-
-# Закрытие Pygame
-pygame.quit()
+    #if enemy_y > WINDOW_HEIGHT:
+       # score += 1
+       # enemy_x = random.randint(0, WINDOW_WIDTH - car_width)
+        #enemy_y = -car_height
